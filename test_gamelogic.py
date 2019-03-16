af549f43e1faff063cb48dd19a3bf4befbfd003c
athul <kpathul@outlook.com>
2019-03-16 13:04:57 +0530

    Red: Adds test for kill()

--- test_gamelogic.py
+++ test_gamelogic.py
@@ -5,3 +5,9 @@ def test_move():
     init_dict = {(0, 300):'r', (60,140):'x'}
     moved_dict = {(1, 300):'r', (61,140):'x'}
     assert gamelogic.move(init_dict) == moved_dict    
+
+def test_kill():
+    init_dict = {(0, 300):'r', (60,140):'x'}
+    killed_dict = {(60,140):'x'}
+    assert gamelogic.kill(init_dict, 'r') == killed_dict
+    assert gamelogic.kill(init_dict, 'a') == init_dict
