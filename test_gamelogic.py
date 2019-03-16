bd0e88e2907bba673ede369debc2150b49bd6bc2
athul <kpathul@outlook.com>
2019-03-16 13:16:15 +0530

    Red: Adds one more test for kill()
    test_kill_onlylowestletter(): gamelogic.kill() should only remove lowest letter

--- test_gamelogic.py
+++ test_gamelogic.py
@@ -11,3 +11,12 @@ def test_kill():
     killed_dict = {(60,140):'x'}
     assert gamelogic.kill(init_dict, 'r') == killed_dict
     assert gamelogic.kill(init_dict, 'a') == init_dict
+
+def test_kill_onlylowestletter():
+    """
+    gamelogic.kill() should only remove the lowest letter.
+    The lowest letter will have the highest row value
+    """
+    init_dict = {(0, 300):'r', (60,140):'x', (210, 500):'r', (200, 100):'r'}
+    killed_dict = {(0, 300):'r', (60,140):'x', (200, 100):'r'}
+    assert gamelogic.kill(init_dict, 'r') == killed_dict
