ed9ae321a6799c605713838dcef08b85ec2a5cda
athul <kpathul@outlook.com>
2019-03-16 18:34:12 +0530

    Adds life() to main()

--- letter_invaders.py
+++ letter_invaders.py
@@ -4,18 +4,29 @@ import curses
 from time import sleep
 from gamelogic import *
 
+
+def max_dimensions(window):
+    height, width = window.getmaxyx()
+    return height - 2, width - 1
+
 def draw(dictionary, window):
     "Draws dictionary in window!"
+    height, width = max_dimensions(window)
     for (row, column), letter in dictionary.items():
+        if row > height or column > width:
+            continue
         window.addch(row, column, letter)
 
 def main(window):
     curses.curs_set(0)
     letters = {}
-    height, width = window.getmaxyx()
+    height, width = max_dimensions(window)
     window.nodelay(True)
-    for t in range(20):
+    while True:
         window.clear()
+        window.border(0)
+        if life(letters, height) < 0:
+            break
         letters = new_letter(width, letters)
         letters = move(letters)
         draw(letters, window)
