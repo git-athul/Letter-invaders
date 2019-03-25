00854460344d7c2a243f0092d198e62ad0b04f43
athul <kpathul@outlook.com>
2019-03-25 08:46:30 +0530

    Updates the tests to the changes

--- test_gamesetup.py
+++ test_gamesetup.py
@@ -3,28 +3,38 @@ from gamesetup import Setup
 
 def test_move():
     "move() should increase the row-value by one"
-    letter_dict = Setup({(0, 300): {'char':'r'}, (60,140):{'char':'x'}})
-    moved_dict = {(1, 300):{'char':'r'}, (61,140):{'char':'x'}}
-    assert letter_dict.move() == moved_dict    
+    letter_dict = Setup({(0, 300): {'char':'r'}, (60, 140):{'char':'x'}})
+    moved_dict = {(1, 300):{'char':'r'}, (61, 140):{'char':'x'}}
+    assert letter_dict.move() == moved_dict
 
-def test_kill():
+def test_input_update():
     """
-    kill() should remove the dictionary item if entered character
+    input_update() should change the dictionary item if entered character
     matches letter in dictionary-value.
     """
-    letter_dict = {(0, 300): {'char':'r'}, (60,140):{'char':'x'}}
-    killed_dict = {(60,140):{'char':'x'}}
-    assert Setup(letter_dict).kill('r') == killed_dict
-    assert Setup(letter_dict).kill('a') == letter_dict
+    letter_dict = {(0, 300): {'char':'r', 'life':False}, (60, 140):{'char':'x', 'life':False}}
+    updated_dict = {(0, 300): {'char':'*', 'life':4}, (60, 140):{'char':'x', 'life':False}}
+    assert Setup(letter_dict).input_update('r') == updated_dict
+    assert Setup(letter_dict).input_update('a') == letter_dict
 
-def test_kill_onlyoneletter():
-    "kill() should only remove the letter with highest row value"
-    letter = {(0, 300): {'char':'r'}, (60,140): {'char':'x'}, (210, 500): {'char':'r'}, (200, 100): {'char':'r'}}
-    letter_dict = Setup(letter)
-    killed_dict = {(0, 300): {'char':'r'}, (60,140): {'char':'x'}, (200, 100): {'char':'r'}}
-    assert letter_dict.kill('r') == killed_dict
+def test_input_update_onlyoneletter():
+    "input_update() should only change the letter with highest row value"
+    letter_dict = Setup({(0, 300): {'char':'r', 'life':False},
+                         (60, 140): {'char':'x', 'life':False},
+                         (210, 500): {'char':'r', 'life':False},
+                         (200, 100): {'char':'r', 'life':False}})
+
+    updated_dict = {(0, 300): {'char':'r', 'life':False},
+                    (60, 140): {'char':'x', 'life':False},
+                    (210, 500): {'char':'*', 'life':4},     #
+                    (200, 100): {'char':'r', 'life':False}}
+    assert letter_dict.input_update('r') == updated_dict
 
 def test_life():
     height = 500
-    letter_dict = Setup({(0, 300):'r', (500,140):'x', (210, 500):'r', (500, 100):'r'})
+    letter_dict = Setup({(0, 300):{'char':'r', 'life':False},
+                         (500, 140):{'char':'x', 'life':False},  #
+                         (500, 130):{'char':'x', 'life':3},
+                         (210, 500):{'char':'r', 'life':False},
+                         (500, 100):{'char':'r', 'life':False}}) #
     assert letter_dict.life(height) == 2
