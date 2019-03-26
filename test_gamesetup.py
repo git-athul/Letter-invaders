d5c4bb506b3494512dd2bfda2e17bee6c19a7403
athul <kpathul@outlook.com>
2019-03-26 10:50:57 +0530

    Green: Adds test for generate_letter()

--- test_gamesetup.py
+++ test_gamesetup.py
@@ -7,6 +7,7 @@ def test_move():
     moved_dict = {(1, 300):{'char':'r'}, (61, 140):{'char':'x'}}
     assert letter_dict.move() == moved_dict
 
+
 def test_update_input():
     """
     update_input() should change the dictionary item if entered character
@@ -18,6 +19,7 @@ def test_update_input():
                     (60, 140):{'char':'x', 'life':False, 'color':2}}
     assert Setup(letter_dict).update_input('r') == (updated_dict, True)
     assert Setup(letter_dict).update_input('a') == (letter_dict, False)
+
 
 def test_update_input_onlyoneletter():
     "update_input() should only change the letter with highest row value"
@@ -31,6 +33,7 @@ def test_update_input_onlyoneletter():
                     (210, 500): {'char':'*', 'life':4, 'color':6},     #
                     (200, 100): {'char':'r', 'life':False, 'color':4}}
     assert letter_dict.update_input('r') == (updated_dict, True)
+
 
 def test_expire_entered():
     "tests whether 'life' is decreasing and expiring for entered"
@@ -53,3 +56,34 @@ def test_count_life():
                          (210, 500):{'char':'r', 'life':False},
                          (500, 100):{'char':'r', 'life':False}}) 
     assert letter_dict.count_life(height, count) == 2
+
+
+def test_generate_letter():
+    "tests the changes in settings"
+    new = Setup({})
+
+    input_settings = {'letter_count':0,
+                      'switch':False, #
+                      'gap_step':6, #
+                      'gap':7,
+                      'level_req':10}
+    _, output_settings = new.generate_letter(100, input_settings)
+    expected = {'letter_count': 0,
+                'switch': True, #
+                'gap_step': 0, #
+                'gap': 7,
+                'level_req': 10}
+    assert expected == output_settings
+
+    input_settings = {'letter_count':6,
+                      'switch':True,
+                      'gap_step':0,
+                      'gap':7,
+                      'level_req':7}
+    _, output_settings = new.generate_letter(100, input_settings)
+    expected = {'letter_count':0,
+                'switch':False,
+                'gap_step':1,
+                'gap':6,
+                'level_req':14}
+    assert expected == output_settings
