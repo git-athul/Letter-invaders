27e37ef13ca4ec68947aaa05aabfa5d71822e234
athul <kpathul@outlook.com>
2019-04-04 09:34:23 +0530

    Corrects the tests

--- test_gamesetup.py
+++ test_gamesetup.py
@@ -3,9 +3,10 @@ from gamesetup import Setup
 
 def test_move():
     "move() should increase the row-value by one"
-    letter_dict = Setup({(0, 300): {'char':'r'}, (60, 140):{'char':'x'}})
+    letter = Setup({(0, 300): {'char':'r'}, (60, 140):{'char':'x'}})
+    letter.move()
     moved_dict = {(1, 300):{'char':'r'}, (61, 140):{'char':'x'}}
-    assert letter_dict.move() == moved_dict
+    assert letter.dictionary == moved_dict
 
 
 def test_update_input():
@@ -13,17 +14,21 @@ def test_update_input():
     update_input() should change the dictionary item if entered character
     matches letter in dictionary-value.
     """
-    letter_dict = {(0, 300): {'char':'r', 'life':False, 'color':1},
-                   (60, 140):{'char':'x', 'life':False, 'color':2}}
+
+    letter = Setup({(0, 300): {'char':'r', 'life':False, 'color':1},
+                   (60, 140):{'char':'x', 'life':False, 'color':2}})
     updated_dict = {(0, 300): {'char':'*', 'life':4, 'color':6},
                     (60, 140):{'char':'x', 'life':False, 'color':2}}
-    assert Setup(letter_dict).update_input('r') == (updated_dict, True)
-    assert Setup(letter_dict).update_input('a') == (letter_dict, False)
-
+    
+    assert letter.update_input('r') == True
+    assert letter.dictionary == updated_dict
+    
+    assert letter.update_input('a') ==  False
+    assert letter.dictionary == updated_dict
 
 def test_update_input_onlyoneletter():
     "update_input() should only change the letter with highest row value"
-    letter_dict = Setup({(0, 300): {'char':'r', 'life':False, 'color':1},
+    letter = Setup({(0, 300): {'char':'r', 'life':False, 'color':1},
                          (60, 140): {'char':'x', 'life':False, 'color':2},
                          (210, 500): {'char':'r', 'life':False, 'color':3},
                          (200, 100): {'char':'r', 'life':False, 'color':4}})
@@ -32,18 +37,21 @@ def test_update_input_onlyoneletter():
                     (60, 140): {'char':'x', 'life':False, 'color':2},
                     (210, 500): {'char':'*', 'life':4, 'color':6},     #
                     (200, 100): {'char':'r', 'life':False, 'color':4}}
-    assert letter_dict.update_input('r') == (updated_dict, True)
+    assert letter.update_input('r') == True
+    assert letter.dictionary == updated_dict
 
 
 def test_expire_entered():
     "tests whether 'life' is decreasing and expiring for entered"
-    letter_dict = Setup({(0, 300): {'char':'*', 'life':3},
+    letter = Setup({(0, 300): {'char':'*', 'life':3},
                          (210, 500): {'char':'*', 'life':1},
                          (200, 100): {'char':'r', 'life':False}})
 
     expired_dict = {(0, 300): {'char':'*', 'life':2},
                    (200, 100): {'char':'r', 'life':False}}
-    assert letter_dict.expire_entered() == expired_dict
+    
+    letter.expire_entered()
+    assert letter.dictionary == expired_dict
 
 
 def test_count_life():
@@ -67,7 +75,7 @@ def test_generate_letter():
                       'gap_step':6, #
                       'gap':7,
                       'level_req':10}
-    _, output_settings = new.generate_letter(100, input_settings)
+    output_settings = new.generate_letter(100, input_settings)
     expected = {'letter_count': 0,
                 'switch': True, #
                 'gap_step': 0, #
@@ -80,7 +88,7 @@ def test_generate_letter():
                       'gap_step':0,
                       'gap':7,
                       'level_req':7}
-    _, output_settings = new.generate_letter(100, input_settings)
+    output_settings = new.generate_letter(100, input_settings)
     expected = {'letter_count':0,
                 'switch':False,
                 'gap_step':1,
